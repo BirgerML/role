86d20cd8350f1ae44c489523242cc870e66b2bce
birger <131198@hig.no>
2015-10-12 17:07:56 +0200

    Fjernet logstashforwarder, legger den til i base

--- manifests/monitor.pp
+++ manifests/monitor.pp
@@ -2,6 +2,5 @@ class role::monitor {
 
   include ::profile::base_linux
   include ::profile::monitoring::elk
-  include ::profile::monitoring::logstashforwarder
   include ::profile::monitoring::icingaserver
 }
