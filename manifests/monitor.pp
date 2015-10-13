65409d736b0ab93ac0b8d34fa74eaa371b2dccfa
birger <131198@hig.no>
2015-10-13 11:46:53 +0200

    la til reverseproxy i monitor-rolle

--- manifests/monitor.pp
+++ manifests/monitor.pp
@@ -2,5 +2,6 @@ class role::monitor {
 
   include ::profile::base_linux
   include ::profile::monitoring::elk
+  include ::profile::monitoring::reverseproxy.pp
 #  include ::profile::monitoring::icingaserver
 }
