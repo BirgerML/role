834f849ed50cc86d0e984c09029262feee687675
BirgerML <BirgerML@users.noreply.github.com>
2015-10-13 11:58:07 +0200

    Update monitor.pp

--- manifests/monitor.pp
+++ manifests/monitor.pp
@@ -2,6 +2,6 @@ class role::monitor {
 
   include ::profile::base_linux
   include ::profile::monitoring::elk
-  include ::profile::monitoring::reverseproxy.pp
+  include ::profile::monitoring::reverseproxy
 #  include ::profile::monitoring::icingaserver
 }
