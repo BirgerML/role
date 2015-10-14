e7b48f74402a8b0b157ab735897196ea0b4ab539
birger <131198@hig.no>
2015-10-14 14:03:59 +0200

    la til icingaserver

--- manifests/monitor.pp
+++ manifests/monitor.pp
@@ -3,5 +3,5 @@ class role::monitor {
   include ::profile::base_linux
   include ::profile::monitoring::elk
   include ::profile::monitoring::reverseproxy
-#  include ::profile::monitoring::icingaserver
+  include ::profile::monitoring::icingaserver
 }
